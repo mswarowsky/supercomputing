295c98e1c61b202468a01d9172323ca8fb890573
Markus Swarowsky <markus.swarowsky@gmail.com>
2019-02-21 11:09:13 +0100

    mach0 single threaded program running

--- mach0/mach0.cpp
+++ mach0/mach0.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <cmath>
+#include <mach.h>
+
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "Hello, World!" << std::endl;
+    if(argc < 2){
+        std::cout << "To less arguments, please provide a n!";
+        return 1;
+    }
+
+    auto n = atoi(argv[1]);
+    std::cout << "Using n = " << n << std::endl;
+
+    auto approx_pi = mach::getPIMachSingleT(n);
+
+    std::cout << "Approximated PI: " << approx_pi << "\n"<< "error: " << fabs(approx_pi - M_PI) ;
+    
     return 0;
 }
