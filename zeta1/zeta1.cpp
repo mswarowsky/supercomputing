dc25e18b9b92540bdf20ac2c0093909885d9ff7b
Markus Swarowsky <markus.swarowsky@gmail.com>
2019-02-22 11:20:22 +0100

    zeta1 MPI first try

--- zeta1/zeta1.cpp
+++ zeta1/zeta1.cpp
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <mpi.h>
 #include <vector>
+#include <numeric>
 
 int main(int argc, char *argv[]) {
 
@@ -16,8 +17,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    size_t n = std::strtoul(argv[1], nullptr, 0);
-    std::cout << "Using n = " << n << std::endl;
 
     // Starting MPI
     int size, rank;
@@ -27,14 +26,31 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &rank);
 
 
+    std::cout << "rank: " << rank << std::endl;
+
+    size_t n = std::strtoul(argv[1], nullptr, 0);
+    if(rank == 0) {
+        std::cout << "Using n = " << n << std::endl;
+    }
+
+
+
+    // To some calculation for splitting the data
+    size_t chunk;
+    if((size > 1) && (n % size != 0)){
+        chunk = n / (size - 1);
+    } else {
+        chunk = n / size;
+    }
+
 
     // compute the vector elements
     std::vector<double> v;
-    v.reserve(n);   //to allocate already all memory needed
+    v.resize(chunk * size);   //to allocate already all memory needed
 
     if(rank == 0){
         for(size_t i = 1; i < n; i++) {
-            v.emplace_back(zeta::zetaElement(i));
+            v.at(i) = zeta::zetaElement(i);
         }
     }
 
@@ -42,15 +58,22 @@ int main(int argc, char *argv[]) {
     if(rank == 0){
         std::cout << "Sending elements to " << size << " other processes.\n";
     }
-    //TODO Broadcast
-    MPI_Bcast(v.data(), n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    //Splitting Data and sending it to all the processes
+    std::vector<double> part_v;
+    part_v.resize(chunk);
+    MPI_Scatter(v.data(), static_cast<int>(chunk), MPI_DOUBLE, part_v.data(), static_cast<int>(chunk), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    //TODO summ the brodcasted elements up ?
-    double approx_pi;
+
+    //sum up the scattered elements
+    double local_pi = std::accumulate(part_v.begin(), part_v.end(), 0.0);
+
+    double global_pi;
+    MPI_Reduce(&local_pi, &global_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
 
 
     if(rank == 0){
-        std::cout << "Approximated PI: " << approx_pi << "\n"<< "error: " << fabs(approx_pi - M_PI) ;
+        std::cout << "Approximated PI: " << global_pi << "\n"<< "error: " << fabs(global_pi - M_PI) ;
     }
 
     MPI_Finalize();
